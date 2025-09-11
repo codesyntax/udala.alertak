6640ad2f9c85a7ac0b42de1e73c298b995ca2ba4
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-09-11 16:18:01 +0200

    Add vocabulary: WhereToShowAlert

---
+++ src/udala/alertak/tests/test_vocab_where_to_show_alert.py
@@ -0,0 +1,31 @@
+from udala.alertak import _
+from udala.alertak.testing import UDALA_ALERTAK_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.interfaces import IVocabularyTokenized
+
+import unittest
+
+
+class WhereToShowAlertIntegrationTest(unittest.TestCase):
+
+    layer = UDALA_ALERTAK_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_vocab_where_to_show_alert(self):
+        vocab_name = 'udala.alertak.WhereToShowAlert'
+        factory = getUtility(IVocabularyFactory, vocab_name)
+        self.assertTrue(IVocabularyFactory.providedBy(factory))
+
+        vocabulary = factory(self.portal)
+        self.assertTrue(IVocabularyTokenized.providedBy(vocabulary))
+        self.assertEqual(
+            vocabulary.getTerm('sony-a7r-iii').title,
+            _(u'Sony Aplha 7R III'),
+        )
